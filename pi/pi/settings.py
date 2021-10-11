80536af47560cd86b06a90613b2af5e67fe8d042
alexsoares4a <73480812+alexsoares4a@users.noreply.github.com>
2021-10-11 17:59:00 -0300

    Ajuste para tornar o menu responsivo para celular

--- pi/pi/settings.py
+++ pi/pi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-50wv%b1ry&0116u+_zx@lq2f#n2!^_j*dxfg073iv#kv&i8^41
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
