2a1185a92faa893764111f5b2af793c9ad673000
alexsoares4a <73480812+alexsoares4a@users.noreply.github.com>
2021-11-14 17:54:21 -0300

    Ajuste para usuário alterar seus dados cadastrados

--- pi/users/admin.py
+++ pi/users/admin.py
@@ -14,3 +14,18 @@ class UserAdmin(auth_admin.UserAdmin):
     add_form = UserCreationForm
     models = User
     inlines = [ContatoInline]
+
+    list_display = ('username', 'email', 'first_name',
+                    'last_name', 'id')
+    search_fields = ('username', 'email', 'first_name', 'last_name', 'id')
+
+    def get_queryset(self, request):
+        # Get the logged in user
+        usuario_logado = User.objects.filter(username=request.user)
+        # Override the get_queryset method for Admin
+        qs = super(UserAdmin, self).get_queryset(request)
+
+        if not request.user.is_superuser:
+            return qs.filter(username=usuario_logado[0])
+        else:
+            return qs
