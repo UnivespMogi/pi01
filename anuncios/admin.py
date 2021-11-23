817890c5a9a5b682abb5b1f3018b33c45a9dc29b
niltonmassao <nilton.uekita@hotmail.com>
2021-11-23 19:04:00 -0300

    arrumando o banco de dados e tirado o contatos do menu prncipal

--- anuncios/admin.py
+++ anuncios/admin.py
@@ -76,38 +76,9 @@ class ServicoAdmin(admin.ModelAdmin):
 
 
 
-class ContatoAdmin(admin.ModelAdmin):
-    model = Contato
-    # exclude = ('dt_cadastro','usuario')
-    list_display = ('tp_contato', 'tx_contato' )
-    fields = ['tp_contato', 'tx_contato']
-    search_fields = ('tp_contato', 'tx_contato')
-
-    #exibir apenas produtos cadastrados pelo usuário logado
-    def get_queryset(self, request):
-        # Get the logged in user
-        usuario_logado = User.objects.filter(username = request.user)
-        # Override the get_queryset method for Admin
-        qs = super(ContatoAdmin, self).get_queryset(request)
-
-        if not request.user.is_superuser:
-            return qs.filter(usuario__in = usuario_logado)
-        else:
-            return qs
-
-    def save_form(self, request, form, change):
-        obj = super().save_form(request, form, change)
-        obj = form.save(commit=False)
-        obj.usuario = request.user
-        if not change:
-            obj.usuario = request.user
-        return obj
-
-
 
 
 #admin.site.register(Condominio, CondominioAdmin)
 #admin.site.register(Categoria)
 admin.site.register(Produto, ProdutoAdmin)
 admin.site.register(Servico, ServicoAdmin)
-admin.site.register(Contato, ContatoAdmin)
